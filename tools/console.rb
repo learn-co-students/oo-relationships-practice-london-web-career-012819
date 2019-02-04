5226a859d475c2b1798bbd7ce82fbf507ee78c32
stubrew24 <sewelly@gmail.com>
2019-02-04 14:04:04 +0000

    IMDB

--- tools/console.rb
+++ tools/console.rb
@@ -17,14 +17,39 @@ end
 # g1.create_trip(l2, "20/02/2019")
 # g1.create_trip(l2, "20/02/2019")
 # g2.create_trip(l1, "20/02/2019")
+#
+# b1 = Bakery.new("Stu Bakes")
+# b2 = Bakery.new("Suzie Cupcakes")
+#
+# d1 = b1.create_dessert("Toffe Apple", ["toffee", "apples"])
+# d2 = b1.create_dessert("Choc Cheesecake", ["choc", "cheese", "biscuits", "butter"])
+# d3 = b2.create_dessert("Choc Cupcake", ["choc", "flour", "sugar"])
 
-b1 = Bakery.new("Stu Bakes")
-b2 = Bakery.new("Suzie Cupcakes")
+a1 = Actor.new("Ben Stiller")
+a2 = Actor.new("Bruce Willis")
+a3 = Actor.new("Chris Pratt")
+a4 = Actor.new("Gary Oldman")
 
-d1 = b1.create_dessert("Toffe Apple", ["toffee", "apples"])
-d2 = b1.create_dessert("Choc Cheesecake", ["choc", "cheese", "biscuits", "butter"])
-d3 = b2.create_dessert("Choc Cupcake", ["choc", "flour", "sugar"])
+c1 = Character.new("Derek Zoolander", a1) #5
+c2 = Character.new("White Goodman", a1) #3
 
+c3 = Character.new("David Dunn", a2) #6
+c4 = Character.new("John McClane", a2) #3
 
+c5 = Character.new("Star-Lord", a3) #3
+c6 = Character.new("Owen", a3) #
+c6 = Character.new("Jim Preston", a3) #33
+
+c7 = Character.new("Com. Gordon", a4) #3
+
+m1 = Movie.new("Avengers", [c1,c3,c5,c7])
+m2 = Movie.new("Revengers", [c1,c3])
+m3 = Movie.new("Lenders", [c2,c4,c6])
+m4 = Movie.new("Spender", [c3,c7])
+m5 = Movie.new("Mender", [c1,c2,c3,c4,c5,c6])
+
+s1 = Show.new("Avengers", [c1,c3,c5,c7])
+s2 = Show.new("Friends", [c1,c3])
+s3 = Show.new("Partridges", [c2,c4,c6])
 
 Pry.start
